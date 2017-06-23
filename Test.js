c8527effaa66e759f1936bce28d624186691db74
MB Center <bankultimate@users.noreply.github.com>
2017-06-23 15:47:30 +0700

    Telnet-Client Test
    Telnet-Client Test

--- Test.js
+++ Test.js
@@ -1,1 +1,29 @@
-console.log('Hello Test');
+console.log('Hello Telnet');
+
+var telnet = require('telnet-client')
+var connection = new telnet()
+
+var params = {
+  host: '127.0.0.1',
+  port: 23,
+  shellPrompt: '/ # ',
+  timeout: 1500,
+  // removeEcho: 4
+}
+
+connection.on('ready', function(prompt) {
+  connection.exec(cmd, function(err, response) {
+    console.log(response)
+  })
+})
+
+connection.on('timeout', function() {
+  console.log('socket timeout!')
+  connection.end()
+})
+
+connection.on('close', function() {
+  console.log('connection closed')
+})
+
+connection.connect(params)
